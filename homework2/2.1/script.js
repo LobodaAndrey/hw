fc519db9a2a7e4ec28534c4c8dd690a49f6741da
LobodaAndrey <lobodaandrey92@gmail.com>
2018-11-02 15:31:11 +0200

    final commit

--- homework2/2.1/script.js
+++ homework2/2.1/script.js
@@ -1,21 +1,36 @@
-var arr = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0],
-	amount = 0;
-
-arr.forEach( function(el, i) {
-	var n = 1;
-
-	while (arr[i] > arr[i+n]) {
-
-		amount = amount + (arr[i]-arr[i+n]);
-		n++
-		
-		//каждый раз консоль лог, чтобы видеть, как растёт сумма
-
-		console.log(amount)
-	} 
-});
+// Actually my knowledge wasnt enough to solve this task. I tried for a few days, but my solution was counting water twice at each column, so I was forced to find a similar method in google to solve this =(
 
 
-console.log(amount)
+var arr = [5, 2, 7, 4, 1, 3];
 
+function waterAmount(array) {
+    var max = Math.max.apply(null, array),
+        mySum = 0;
 
+        //found max point
+        console.log(max)
+
+    for (var i = 1; i <= max; i++) {
+        var inside = false,
+            gaps = 0;
+        
+        for (var j = 0; j < arr.length; j++) {
+            if (arr[j] >= i) {
+                if (inside) {
+                    inside = false;
+                    mySum += gaps;
+                    j--;
+                } else {
+                    inside = true;
+                    gaps = 0;
+                }
+            } else {
+                gaps++;
+            }
+        }
+    }
+    
+    return mySum;
+}
+
+console.log(waterAmount(arr))
