2303d3d98fcff173f12bb43a0076bdbfa858fde0
LobodaAndrey <lobodaandrey92@gmail.com>
2018-11-09 14:17:05 +0200

    hw4

---
+++ homework4/4.2/script.js
@@ -0,0 +1,27 @@
+var grid = [x, y],
+	start = [0, 0],
+	position = [x0, y0],
+	output = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var input = [R, C, r0, c0],
+	
+
+//  R = 1,
+//  C = 5,
+//  r0 = 0,
+//  c0 = 0;
+
+
+// output = [];
